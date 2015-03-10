bad81124b2f07d4b9e3464b86a9c3a48e8d90005
Rabka <smot@itu.dk>
2015-03-10 13:34:22 +0100

    Added manager

---
+++ RESTfulWorkflow/Manager/Manager.fs
@@ -0,0 +1,16 @@
+﻿
+let test r = 
+    let port = 8080
+    let url = sprintf "http://localhost:8080/resource/%s" r
+    use w = new System.Net.WebClient () 
+
+    w.UploadString(url, "CREATE", "value for A") |> printfn "CREATE resource/A --> %s"
+    w.DownloadString(url)                        |> printfn "GET /resource/A --> %s" 
+    w.UploadString(url, "PUT", "new value")      |> printfn "PUT /resource/A [new value] --> %s"
+    w.DownloadString(url)                        |> printfn "GET /resource/A --> %s"
+
+[<EntryPoint>]
+let main argv =
+    test "A"
+    test "B"
+    0
