02e4b8fd9e2cf8b73675d9c5ddf16063baab8eaa
Patrick Evers Bjørkman <pebj@itu.dk>
2015-03-17 13:50:41 +0100

    TestClient seasoning
    Can survive network issues and provides a general workflow in terms of
    giving the users a choice of what they want to do. This includes giving
    the choice to exit the program by keyboard if you are too lazy to use
    the mouse pointer..

--- RESTfulWorkflow/TestClient/Client.fs
+++ RESTfulWorkflow/TestClient/Client.fs
@@ -2,47 +2,77 @@
 
 // --debois, Mar '15
 let HTTPRequestDownload (url : string) = 
-    let port = 8080
-    use w = new System.Net.WebClient () 
-    w.DownloadString(url)
+    try
+        let port = 8080
+        use w = new System.Net.WebClient () 
+        Some(w.DownloadString(url))
+    with
+        | _ -> None
 
 let HTTPRequestUpload (url : string) verb value = 
-    let port = 8080
-    use w = new System.Net.WebClient () 
-    w.UploadString(url,verb, value)
-
-let rec mainLoop baseUrl =
-    printfn "Please provide event name:"
+    try
+        let port = 8080
+        use w = new System.Net.WebClient () 
+        Some(w.UploadString(url,verb, value))
+    with
+        | _ -> None
+        
     
-    //http://localhost:8080/<process>/<session_id>/<event>/<attribute>
-    let eventName = Console.ReadLine()
-    let executed = HTTPRequestDownload (baseUrl + "/" + eventName + "/executed")
-    let included = HTTPRequestDownload (baseUrl + "/" + eventName + "/included")
-    let pending = HTTPRequestDownload (baseUrl + "/" + eventName + "/pending")
-
-    //Write out status:
-    printfn "Event status:"
-    printfn "IsExecuted: %s" executed 
-    printfn "IsIncluded: %s" included
-    printfn "IsPending: %s" pending
+let rec mainLoop baseUrl =
     printfn "Actions:"
-    printfn "1 -> Execute"
-    printfn "2 -> Include"
-    printfn "3 -> Pending"
-    printfn "4 -> Lock"
-    printfn "5 -> Exit event"
-    printfn "6 -> Exit program"
+    printfn "1 -> Connect an event"
+    printfn "2 -> Change BaseURL"
+    printfn "3 -> Exit program"
+    printf "-> "
     let action = Console.ReadLine()
-    if (action = "1") then
-        HTTPRequestUpload (baseUrl + "/" + eventName + "/executed") "PUT" "true" |> printfn "Result: %s"
-    if (action = "2") then
-        HTTPRequestUpload (baseUrl + "/" + eventName + "/included") "PUT" "true" |> printfn "Result: %s"
     if (action = "3") then
-        HTTPRequestUpload (baseUrl + "/" + eventName + "/pending") "PUT" "true" |> printfn "Result: %s"
-    if (action = "4") then
-        HTTPRequestUpload (baseUrl + "/" + eventName + "/locked") "PUT" "true" |> printfn "Result: %s"         
-    if not (action = "6") then
-        mainLoop baseUrl     
+        None    
+    else   
+        if (action = "2") then
+            Some(true)
+        else
+            printfn "Please provide event name:"
+            let eventName = Console.ReadLine()
+            let executed = HTTPRequestDownload (baseUrl + "/" + eventName + "/executed")
+            let included = HTTPRequestDownload (baseUrl + "/" + eventName + "/included")
+            let pending = HTTPRequestDownload (baseUrl + "/" + eventName + "/pending")
+            if (executed.IsNone || included.IsNone || pending.IsNone) then
+                printfn "Program failed to exchange data with the event. Eventname, baseURL or connection may be at fault."
+                mainLoop baseUrl
+            else
+                //Write out status:
+                printfn "Event status:"
+                printfn "IsExecuted: %s" executed.Value 
+                printfn "IsIncluded: %s" included.Value 
+                printfn "IsPending: %s" pending.Value 
+                printfn "Actions:"
+                printfn "1 -> Execute"
+                printfn "2 -> Include"
+                printfn "3 -> Pending"
+                printfn "4 -> Lock"
+                printfn "5 -> Exit event"
+                printf "-> "
+                let action = Console.ReadLine()
+                printfn "Executing action"
+                let mutable response = None
+                if (action = "1") then
+                    response <- HTTPRequestUpload (baseUrl + "/" + eventName + "/executed") "PUT" "true"
+                else
+                    if (action = "2") then
+                        response <-  HTTPRequestUpload (baseUrl + "/" + eventName + "/included") "PUT" "true"
+                    else
+                        if (action = "3") then
+                            response <-  HTTPRequestUpload (baseUrl + "/" + eventName + "/pending") "PUT" "true"
+                        else
+                            if (action = "4") then
+                                response <-  HTTPRequestUpload (baseUrl + "/" + eventName + "/locked") "PUT" "true"       
+                            else
+                                if (response.IsNone) then
+                                    printfn "Program failed to issue the command to the event. Connection may be at fault."    
+                if (response.IsSome) then 
+                    response.Value |> printfn "Success! Response: %s" 
+                printfn "action completed"
+                mainLoop baseUrl 
 
 let GetWorkFlowUrl url = 
     if (not (Uri.IsWellFormedUriString(url, UriKind.Absolute))) then
@@ -60,12 +90,18 @@ let rec SelectBaseUrl n =
     else
         printfn "Entered url was invalid, please try again."
         SelectBaseUrl n
+let rec outerLoop m = 
+    let baseUrl = SelectBaseUrl 0
+    let m = mainLoop baseUrl
+    if (m.IsSome) then
+        outerLoop m
 
 [<EntryPoint>]
-let main argv = 
+let rec main argv = 
+    printfn "Welcome to the testclient of the workflow client"
     Console.Title <- "Workflow test client"
-    let baseUrl = SelectBaseUrl 0
-    mainLoop baseUrl
+    //http://localhost:8080/<process>/<session_id>/<event>/<attribute>
+    outerLoop None
     0
 
         
