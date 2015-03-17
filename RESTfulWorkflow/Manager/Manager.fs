29c49089883d12d11e7e5d70305e13dbec932116
Rabka <smot@itu.dk>
2015-03-17 13:51:42 +0100

    Update manager

--- RESTfulWorkflow/Manager/Manager.fs
+++ RESTfulWorkflow/Manager/Manager.fs
@@ -1,25 +1,19 @@
 ﻿
 let port = "8080"
-let serverName = "Midlertidigt navn"
+let serverName = "MidlertidigtNavn"
 let url = sprintf "http://localhost:%s/%s" port serverName
 
-let createEvent name =
+let create (url : string) data =
     use w = new System.Net.WebClient ()
-    let url = (sprintf " %s/%s" url name)
-    w.UploadString(url, "CREATE")
-        |> printfn "CREATE %s/%s --> %s" serverName name
-
-let createRelen fromEvent reTyp toEvent =
-    use w = new System.Net.WebClient ()
-    let url = (sprintf " %s/%s" url fromEvent)
-    w.UploadString(url, "CREATE", (sprintf "%s %s" reTyp toEvent)) 
-        |> printfn "CREATE %s/%s/%s [%s] --> %s" serverName fromEvent reTyp toEvent
+    if not (data = "")
+    then w.UploadString(url, "CREATE", data) |> printfn "CREATE %s [%s] --> %s" url data
+    else w.UploadString(url, "CREATE")       |> printfn "CREATE %s --> %s" url
 
 let parse (line : string) =
     let words = List.ofArray(line.Split [|' '|])
     match words with
-        | "Event"::name::[]                     -> createEvent name
-        | "Relen"::event::typ::toEvent::[]      -> createRelen event typ toEvent
+        | "Event"::name::[]                     -> create (sprintf " %s/%s" url name) ""
+        | "Relen"::event::typ::toEvent::[]      -> create (sprintf " %s/%s/%s" url event typ) toEvent
         | x                                     -> printfn "%s --> Is not parseble" (List.fold (fun acc x -> acc + x + " ") "" x)
 
 [<EntryPoint>]
@@ -33,6 +27,6 @@ let main argv =
 
     parse "Event A"
     parse "Event B"
-    parse "Relen A Condition B"
+    parse "Relen A condition B"
 
     0 // <- skal vare der
