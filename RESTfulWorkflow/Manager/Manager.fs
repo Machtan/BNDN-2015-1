b60d0de9d690107838d7191f1160bed7b433ee25
Rabka <smot@itu.dk>
2015-03-20 09:56:28 +0100

    Komentated manager

--- RESTfulWorkflow/Manager/Manager.fs
+++ RESTfulWorkflow/Manager/Manager.fs
@@ -1,32 +1,51 @@
 ﻿
 let port = "8080"
-let serverName = "MidlertidigtNavn"
+let serverName = "Test"
 let url = sprintf "http://localhost:%s/%s" port serverName
 
-let create (url : string) data =
+// POST new events and ralentienships to the server
+let post (url : string) data =
     use w = new System.Net.WebClient ()
     if not (data = "")
-    then w.UploadString(url, "POST", data) |> printfn "CREATE %s [%s] --> %s" url data
-    else w.UploadString(url, "POST")       |> printfn "CREATE %s --> %s" url
+    then w.UploadString(url, "POST", data) |> printfn "POST %s [%s] --> %s" url data
+    else w.UploadString(url, "POST")       |> printfn "POST %s --> %s" url
 
+// parse's a string into a spisifik post to the server
 let parse (line : string) =
-    let words = List.ofArray(line.Split [|' '|])
+    let words = List.ofArray(line.Split ' ')
     match words with
-        | "Event"::name::[]                     -> create (sprintf " %s/%s" url name) ""
-        | "Relen"::event::typ::toEvent::[]      -> create (sprintf " %s/%s/%s" url event typ) toEvent
+        | "Event"::name::[]                     -> post (sprintf " %s/%s" url name) ""
+        | "Relen"::event::typ::toEvent::[]      -> post (sprintf " %s/%s/%s" url event typ) toEvent
         | x                                     -> printfn "%s --> Is not parseble" (List.fold (fun acc x -> acc + x + " ") "" x)
 
 [<EntryPoint>]
 let main argv =
-    
-    
+    //Starts the server form the .exe fil server form same plasments as the program
     let p = new System.Diagnostics.Process()
     p.StartInfo.FileName <- "server.exe"
     p.StartInfo.Arguments <- (serverName + " " + port)
     p.Start() |> ignore
 
-    parse "Event A"
-    parse "Event B"
-    parse "Relen A condition B"
+    //A test identical to the one given i the project decripion
+    parse "Event register"
+    parse "Event pass"
+    parse "Event fail"
+
+    parse "Relen register exclusion register"
+    parse "Relen register condition pass"
+    parse "Relen register response pass"
+    parse "Relen register condition fail"
+
+    parse "Relen pass exclusion pass"
+    parse "Relen pass exclusion fail"
+
+    parse "Relen fail exclusion fail"
+    parse "Relen fail exclusion pass"
+
+    //test
+    use w = new System.Net.WebClient ()
+    printfn "%s" (w.DownloadString(url))
+    System.Console.ReadLine |> ignore
+
 
     0 // <- skal vare der
