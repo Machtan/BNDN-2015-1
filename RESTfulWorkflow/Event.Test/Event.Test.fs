04f5529d1a896f0ab79db340aa0cc877b994a107
Rabka <smot@itu.dk>
2015-03-20 13:22:59 +0100

    Update

--- RESTfulWorkflow/Event.Test/Event.Test.fs
+++ RESTfulWorkflow/Event.Test/Event.Test.fs
@@ -1,4 +1,4 @@
-﻿module Lib
+﻿module test
 
 //From exsemplet in class
 open NUnit.Framework
---
+++ RESTfulWorkflow/RESTfulWorkflow/Client.fs
@@ -0,0 +1,22 @@
+﻿module Client
+
+let test r = 
+    let port = 8080
+    let url = sprintf "http://localhost:8080/resource/%s" r
+    use w = new System.Net.WebClient () 
+
+    w.UploadString(url, "CREATE", "value for A") |> printfn "CREATE resource/A --> %s"
+    w.DownloadString(url)                        |> printfn "GET /resource/A --> %s" 
+    w.UploadString(url, "PUT", "new value")      |> printfn "PUT /resource/A [new value] --> %s"
+    w.DownloadString(url)                        |> printfn "GET /resource/A --> %s" 
+
+(*
+[<EntryPoint>]
+let main _ = 
+    printfn "Waiting for server to come up ..."
+    System.Threading.Thread.Sleep 2000
+    test "A"
+    test "B"
+    0 // <-- *)
+
+// --debois, Mar '15
