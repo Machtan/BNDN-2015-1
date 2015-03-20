449145e83975e35b50b4e657e8a5a7a26f70cd31
Christopher Blundell <christopher.n.blundell@gmail.com>
2015-03-20 22:21:40 +0100

    Initial attempt at reading files from a text file. Can read, but passing them as arguments is missing.

--- RESTfulWorkflow/Manager/Manager.fs
+++ RESTfulWorkflow/Manager/Manager.fs
@@ -1,4 +1,5 @@
-﻿
+﻿open System.IO
+
 let port = "8080"
 let serverName = "MidlertidigtNavn"
 let url = sprintf "http://localhost:%s/%s" port serverName
@@ -15,6 +16,13 @@ let parse (line : string) =
         | "Event"::name::[]                     -> create (sprintf " %s/%s" url name) ""
         | "Relen"::event::typ::toEvent::[]      -> create (sprintf " %s/%s/%s" url event typ) toEvent
         | x                                     -> printfn "%s --> Is not parseble" (List.fold (fun acc x -> acc + x + " ") "" x)
+
+//Her kan vi iterere gennem arrayet med linjer fra text filen.
+//File.ReadAllLines("test.txt") |> Array.iteri (fun i line -> printfn "%d> %s" (i + 1) line) //Reads each line into an array. Thereafter iterates through the list and prints each line.
+
+//Atleast i tried lal.
+//File.ReadAllLines("test.txt") |> Array.iteri (fun i line -> parse"(i + 1) line")  //Tænker det er noget i denne retning der skal bruges.
+
 
 [<EntryPoint>]
 let main argv =
