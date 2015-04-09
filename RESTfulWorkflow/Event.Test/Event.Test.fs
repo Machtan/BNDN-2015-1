32d1f09868ef9984af6a5dd38ff9e41811ff0ccb
Rabka <smot@itu.dk>
2015-04-09 09:41:19 +0200

    Updated tests

--- RESTfulWorkflow/Event.Test/Event.Test.fs
+++ RESTfulWorkflow/Event.Test/Event.Test.fs
@@ -54,17 +54,17 @@ type public Test() =
 
         testUpload w "http://localhost:8080/Test?action=reset" "PUT" "TestClient"
 
-        testUpload w "http://localhost:8080/Test/Event1" "POST" "000 Testw"
-        testUpload w "http://localhost:8080/Test/Event2" "POST" "000 Testw"
-        testUpload w "http://localhost:8080/Test/Event3" "POST" "000 Testw"
-        testUpload w "http://localhost:8080/Test/Event4" "POST" "000 Testw"
-        testUpload w "http://localhost:8080/Test/Event5" "POST" "000 Testw"
+        testUpload w "http://localhost:8080/Test/Event1" "POST" "000 TestClient"
+        testUpload w "http://localhost:8080/Test/Event2" "POST" "000 TestClient"
+        testUpload w "http://localhost:8080/Test/Event3" "POST" "000 TestClient"
+        testUpload w "http://localhost:8080/Test/Event4" "POST" "000 TestClient"
+        testUpload w "http://localhost:8080/Test/Event5" "POST" "000 TestClient"
 
         testUpload w "http://localhost:8080/Test/Event1/exclusion" "POST" "Event2" // [1]->%[2]
         testUpload w "http://localhost:8080/Test/Event1/condition" "POST" "Event3" // [1]->o[3]
         testUpload w "http://localhost:8080/Test/Event1/response"  "POST" "Event5" // [1]o->[5]
         testUpload w "http://localhost:8080/Test/Event2/condition" "POST" "Event3" // [2]->o[3]
-        testUpload w "http://localhost:8080/Test/Event4/inclusion"   "POST" "Event2" // [4]->+[2]
+        testUpload w "http://localhost:8080/Test/Event4/inclusion" "POST" "Event2" // [4]->+[2]
 
         // [1]->%[2], [1]->o[3], [1]o->[5]
         // [2]->o[3]
