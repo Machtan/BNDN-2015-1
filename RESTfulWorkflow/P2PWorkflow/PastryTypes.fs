b96dcdb77cc6e3b98c72fba0077a4198ba75c204
Patrick Evers Bjørkman <pebj@itu.dk>
2015-05-05 13:52:52 +0100

    RestAPI eventlogic and workflow partially implemented

---
+++ RESTfulWorkflow/P2PWorkflow/PastryTypes.fs
@@ -0,0 +1,66 @@
+module PastryTypes
+
+open System
+open System.Net
+
+// =================== TYPE DEFINITIONS ======================
+type U128 = {
+    a: uint64; // First part of the 128 bits
+    b: uint64; // Second part of the 128 bits
+}
+type NetworkLocation = string // an url... Maybe something better later
+type GUID = U128 // u128... Maybe something better later
+type Address = NetworkLocation
+
+// NOTE NOTE NOTE
+// When you UPDATE this:
+// Also update 'send_messsage' and 'start_listening.listen'
+type MessageType =
+| Join      // A new node is joining and requesting states
+| Update    // A new node has been added sucessfully: update to include it
+| JoinState // The state data for a new node
+// Request a resource somewhere to be routed back using the context
+| Resource of string * string
+    // Add more persistency commands here
+
+// The state record of a Pastry node
+type Node = {
+    guid: GUID;
+    address: NetworkLocation;
+    neighbors: Map<GUID, Address>;  // Physical location proximity (IP address)
+    leaves: Map<GUID, Address>;     // GUID-numerically closest nodes
+    minleaf: U128;
+    maxleaf: U128;
+    routing_table: Map<GUID, Address> list;
+}
+
+// Something to make the JSON serialization work
+type SerializableNode = {
+    guid: string;
+    address: NetworkLocation;
+    routing_table: Map<string, Address> list;
+    minleaf: string;
+    maxleaf: string;
+    neighbors: Map<string, Address>;
+    leaves: Map<string, Address>;
+}
+
+// A result type to simplify the interpretation a little
+type InterpretResult<'a> =
+| Valid of Node * 'a
+| Invalid of string * int * string
+
+type Destination =
+| Ok of GUID
+| Error of string * int * string
+
+// Updated state, response, status code
+type ResourceResponse<'a> = 'a * string * int
+
+// A function for the resource request func to send requests through
+// partial_resource_url, method, data, state -> response
+type SendFunc<'a> = string -> string -> string -> 'a -> ResourceResponse<'a>
+
+// A function to handle resource requests
+// url, method, send_func, state -> response
+type ResourceRequestFunc<'a> = string -> string -> SendFunc<'a> -> 'a -> ResourceResponse<'a>
