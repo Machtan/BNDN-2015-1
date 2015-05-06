fc789f3320832bfd5156575322f32a66ea2f36dd
unknown <aslak_vanggaard@hotmail.com>
2015-05-06 15:14:15 +0200

    begin fixing WFLogic after interface change

--- RESTfulWorkflow/P2PWorkflow/WorkflowLogic.fs
+++ RESTfulWorkflow/P2PWorkflow/WorkflowLogic.fs
@@ -2,17 +2,29 @@
 
 open Repository_types
 
+//Get
+// let get_events (wfName : WorkflowName)(repos : Repository) : EventName list =
+//    let events = 
+//        match repos.workflows.TryFind(wfName) with
+//        | Some(v) -> v
+//        | None -> failwith "Noooooo" 
+//    let (makeEventnames : EventName list) =
+//        events |> List.map (fun (event) -> (wfName, event))
+//    makeEventnames(events)
+
 //Create
 
 /// Creates and returns a new workflow from a name
-let create_workflow (workflow : WorkflowName) : ResultWorkflow =
-    failwith "Not implemented yed."
-    ResultWorkflow.Ok(workflow,[])
+let create_workflow (wfName : WorkflowName) (repos : Repository) : Result =
+    let workflows = repos.workflows.Add(wfName, [])
+    Result.Ok(repos)
 
 //Read
+let check_workflow (wfName : WorkflowName) (repos : Repository) : bool =
+    failwith "Not implemented yed."
 
 /// Metodes used when finding all executabel event for a user
-let find_executable_with_roles (workflow : Workflow) (roles : Roles) : ExecutableInWorkflow =
+let find_executable_with_roles (workflow : Workflow) (roles : Roles) (magic : SendFunc<'a>) (repos : Repository) : ExecutableInWorkflow =
 //    let name, eventList = workflow
 //    let rec findEvents
 //        match eventList with
@@ -24,15 +36,20 @@ let find_executable_with_roles (workflow : Workflow) (roles : Roles) : Executabl
 //Update
 
 /// Adds a given event to a given Workflow and returns the result
-let add_event (workflow : Workflow) (event : EventName) : ResultWorkflow =
-    failwith "Not implemented yed."
-    let wfName, eventNameList = workflow
-    ResultWorkflow.Ok(wfName, event::eventNameList)
+let add_event (wfName : WorkflowName) (event : EventName) (repos : Repository) : ResultWorkflow =
+    let events = 
+        match repos.workflows.TryFind(wfName) with
+        | Some(v) -> v
+        | None -> failwith "Noooooo" 
+    let (makeEventnames : EventName list) =
+        events |> List.map (fun (event) -> (wfName, event))
+    ResultWorkflow.Ok(wfName, event::makeEventnames)
+    //does this meathod rly add a event to the repos?
 
 //Delete
 
 /// Removes given event form given workflow and returns the result
-let remove_event (workflow : Workflow) (event : EventName) : ResultWorkflow =
+let remove_event (workflow : WorkflowName) (event : EventName) (magic : SendFunc<'a>) (repos : Repository) : ResultWorkflow =
     let wfName, eventNameList = workflow
     let rec remove_first eventNameList event =
         match eventNameList with
@@ -42,12 +59,12 @@ let remove_event (workflow : Workflow) (event : EventName) : ResultWorkflow =
     ResultWorkflow.Ok(wfName, (remove_first eventNameList event))
 
 /// Deletes given workflow and returns it if its susesful
-let delete_workflow (workflow : Workflow) : ResultWorkflow =
+let delete_workflow (workflow : Workflow) (magic : SendFunc<'a>) (repos : Repository) : ResultWorkflow =
     let wfName, eventNameList = workflow
     let rec deleteEvents eventNameList=
         match eventNameList with
         | h::eventNameList ->
-            match remove_event workflow h with 
+            match remove_event workflow h magic repos with  
             |ResultWorkflow.Ok(x) -> 
                 let wfN, eventNList = x
                 deleteEvents eventNList
