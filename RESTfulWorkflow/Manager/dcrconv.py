ca9f66ffe51aad6da5f938b8af2a9ed876c5a2c3
Jakob Lautrup Nysom <jaln@itu.dk>
2015-05-09 23:39:26 +0200

    fix: changed test workflow and converter to support that we now use 1 for true for included as well

--- RESTfulWorkflow/Manager/dcrconv.py
+++ RESTfulWorkflow/Manager/dcrconv.py
@@ -16,36 +16,36 @@ EVENTNAME       = "eve"
 
 # role: Role <name>
 # create: Event <name> <Role>
-# relation: Relen <eventname> <type> <eventname> 
+# relation: Relen <eventname> <type> <eventname>
 
 def dcrconv(file):
     """docstring for dcrconv"""
     tree = ET.parse(file)
     root = tree.getroot()
     lines = [] # Lines for the new file
-    
+
     events = {}
-    
+
     specs = root[0]
     resources = specs[0]
     constraints = specs[1]
-    
+
     for role in resources.find("custom").find("roles"):
         name = role.text.replace(" ", "_")
         lines.append("{} {}".format(ROLENAME, name))
-    
+
     for event in resources.find("events"):
         eid = event.attrib["id"]
         roles = [role.text.replace(" ", "_") for role in event.find("custom").find("roles")]
         #print("Adding {} to {}".format(roles, eid))
         roletext = " ".join(roles)
         events[eid] = [roletext]
-    
+
     for label in resources.find("labelMappings"):
         eid = label.attrib["eventId"]
         labtext = label.attrib["labelId"].replace("\n", " ").replace(" ", "_")
         events[eid].append(labtext)
-    
+
     # Find event states
     # excluded, pending, executed => 000
     marking = root.find("runtime").find("marking")
@@ -53,24 +53,24 @@ def dcrconv(file):
     executed = {n.attrib["id"] for n in marking.find("executed")}
     pending = {n.attrib["id"] for n in marking.find("pendingResponses")}
     for eid in events:
-        inc = "0" if eid in included else "1" # Default to true
+        inc = "1" if eid in included else "0"
         pen = "1" if eid in pending else "0"
         exe = "1" if eid in executed else "0"
         events[eid].append(inc + pen + exe)
-    
+
     #pprint.pprint(events)
     for event, attrs in events.items():
         role = attrs[0]
         name = attrs[1]
         state = attrs[2]
         lines.append("{} {} {} {}".format(EVENTNAME, state, name, role))
-    
+
     for supertag, relname in namemap.items():
         for tag in constraints.find(supertag):
             source = events[tag.attrib["sourceId"]][1]
             target = events[tag.attrib["targetId"]][1]
             lines.append("{} {} {} {}".format(RELATIONNAME, source, relname, target))
-    
+
     for line in lines:
         print(line)
 
